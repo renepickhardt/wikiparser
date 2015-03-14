112925565028a1075781fbe539cbd3d5a739ede6
Rene Pickhardt <rene@rene-pickhardt.de>
2015-03-14 08:50:31 +0100

    hello world project with maven. do  compile and  exec:java on your command line to run. to import in eclipse install the eclipse maven project and import the project and then right click --> configure --> configure as maven

---
+++ src/main/java/de/renepickhardt/imessages/wikiparser/App.java
@@ -0,0 +1,13 @@
+package de.renepickhardt.imessages.wikiparser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
